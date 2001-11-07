46a842e16b8b57faf0930b7d8add158931a153c9
mdoar <mdoar>
2001-11-07 00:47:44 +0000

    Updated scripts to copy black.gif

--- build/runtests.bat
+++ build/runtests.bat
@@ -29,12 +29,14 @@ javadoc -private -excludeclass private -excludemember private -J-Xmx128m -doclet
 REM Generate an HTML report comparing the old and new APIs
 javadoc -J-Xmx128m -doclet jdiff.JDiff -docletpath ..\src -d newdocs -stats -oldapi "Old Test API" -newapi "New Test API" -javadocold "../../olddocs/" -javadocnew "../../newdocs/" -sourcepath ..\lib Null
 copy ..\lib\background.gif newdocs
+copy ..\lib\black.gif newdocs
 
 REM Generate another HTML report comparing the old and new APIs, but ignoring 
 REM changes in documentation by using -nodocchanges
 mkdir newdocs2
 javadoc -J-Xmx128m -doclet jdiff.JDiff -docletpath ..\src -d newdocs2 -nodocchanges -stats -oldapi "Old Test API" -newapi "New Test API" -javadocold "../../olddocs/" -javadocnew "../../newdocs/" -sourcepath ..\lib Null
 copy ..\lib\background.gif newdocs2
+copy ..\lib\black.gif newdocs2
 
 cd ..\build
 
