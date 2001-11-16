af488fcd43bdac35c18fa8544e1a4b471f77204e
mdoar <mdoar>
2001-11-16 06:02:14 +0000

    Added use of -baseURI option

--- examples/runme.bat
+++ examples/runme.bat
@@ -1,7 +1,12 @@
-REM This batch file will generate Javadocs for each version of the 
+REM This batch file will generate Javadoc HTML for each version of the 
 REM SuperProduct source code. Then it generates XML from each version, 
 REM and finally compares the two XML files to produce an HTML report describing
 REM every change in the API between the two releases of SuperProduct.
+
+set BASE_URI=http://www.w3.org
+REM You only need to uncomment this next line and change it to where JDiff 
+REM lib is installed on your machine if you are not connected to the Internet.
+REM set BASE_URI=file:///C:/jdiff/lib
 
 REM Make sure we can find xerces.jar for XML parsing
 setlocal 
@@ -24,15 +29,15 @@ javadoc -sourcepath ..\..\SuperProduct2.0 -doctitle "SuperProduct 2.0 API Docume
 cd ..
 
 REM STEP THREE. Generate XML for the old API.
-javadoc -J-Xmx128m -doclet jdiff.JDiff -docletpath ..\..\lib\jdiff.jar -apiname "SuperProduct 1.0" -sourcepath ..\SuperProduct1.0 %OLDPKGS%
+javadoc -J-Xmx128m -doclet jdiff.JDiff -docletpath ..\..\lib\jdiff.jar -apiname "SuperProduct 1.0" -baseURI "%BASE_URI%" -sourcepath ..\SuperProduct1.0 %OLDPKGS%
 
 REM STEP FOUR. Generate XML for the new API
-javadoc -J-Xmx128m -doclet jdiff.JDiff -docletpath ..\..\lib\jdiff.jar -apiname "SuperProduct 2.0" -sourcepath ..\SuperProduct2.0 %NEWPKGS%
+javadoc -J-Xmx128m -doclet jdiff.JDiff -docletpath ..\..\lib\jdiff.jar -apiname "SuperProduct 2.0" -baseURI "%BASE_URI%" -sourcepath ..\SuperProduct2.0 %NEWPKGS%
 
 REM STEP FIVE. Generate HTML report comparing the old and new APIs
 javadoc -J-Xmx128m -doclet jdiff.JDiff -docletpath ..\..\lib\jdiff.jar -d newdocs -stats -oldapi "SuperProduct 1.0" -newapi "SuperProduct 2.0" -javadocold "../../olddocs/" -javadocnew "../../newdocs/" ..\..\lib\Null.java
 copy ..\..\lib\background.gif newdocs
-copy ..\..\lib\black.gif newdocs\changes
+copy ..\..\lib\black.gif newdocs\changes\black.gif
 
 cd ..
 
