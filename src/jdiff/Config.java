eb4988a438751e8f152b7d8bf1a927eb7d13e0c8
mdoar <mdoar>
2001-12-10 06:08:39 +0000

    Work on an XML configuration file

---
+++ src/jdiff/Config.java
@@ -0,0 +1,20 @@
+package jdiff;
+
+import java.io.*;
+import java.util.*;
+
+/** 
+ * Class to handle configuration files for Javadoc and Jdiff.
+ *
+ * See the file LICENSE.txt for copyright details.
+ * @author Matthew Doar, doar@pobox.com
+ */
+public class Config {
+
+    /** Default constructor. */
+    public Config() {
+    }
+
+    /** Set to enable increased logging verbosity for debugging. */
+    private static boolean trace = false;
+}
