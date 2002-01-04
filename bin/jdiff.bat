b46340d6b14f270812900bf92d3b76886146a371
mdoar <mdoar>
2002-01-04 05:11:49 +0000

    Added csh script for jdiff. Using handy trick to get location of the NT script.

--- bin/jdiff.bat
+++ bin/jdiff.bat
@@ -1,19 +1,21 @@
 @echo off
 
 setlocal
-REM Environment variables can be set here or picked up from the environment.
 REM Set JAVA_HOME to the root of your local Java installation, e.g. C:\jdk1.3
-REM set JAVA_HOME=C:\j2sdk1.4.0-beta3
-if "x%JAVA_HOME%" == "x" set JAVA_HOME=C:\local\jdk1.3.x
-
-REM Set JDIFF_INSTALL to the location where JDiff is installed, e.g. C:\jdiff
-if "x%JDIFF_INSTALL%" == "x" set JDIFF_INSTALL=C:\jdiff
+REM Environment variables can be set here, or picked up from the environment.
+if "x%JAVA_HOME%" == "x" set JAVA_HOME=C:\jdk1.3.x
 
 REM You should not need to change anything below this line.
+REM 
+REM %~dp0 is the name of the directory where this script is located
+REM If the value is not correct, set JDIFF_INSTALL, to C:\jdiff for example.
+set DEFAULT_JDIFF_INSTALL=%~dp0
+set DEFAULT_JDIFF_INSTALL=%DEFAULT_JDIFF_INSTALL%\..
+if "x%JDIFF_INSTALL%" == "x" set JDIFF_INSTALL=%DEFAULT_JDIFF_INSTALL%
+
 REM This adds xerces.jar and ant.jar to the classpath, in case they are not 
 REM there already.
-REM set CLASSPATH=%JAVA_HOME%\lib\tools.jar;%JDIFF_INSTALL%\lib\xerces.jar;%JDIFF_INSTALL%\lib\ant.jar;%JDIFF_INSTALL%\lib\jdiff.jar;%CLASSPATH%
-set CLASSPATH=%JAVA_HOME%\lib\tools.jar;%JDIFF_INSTALL%\lib\xerces.jar;%JDIFF_INSTALL%\lib\ant.jar;%JDIFF_INSTALL%\src;%CLASSPATH%
+set CLASSPATH=%JAVA_HOME%\lib\tools.jar;%JDIFF_INSTALL%\lib\xerces.jar;%JDIFF_INSTALL%\lib\ant.jar;%JDIFF_INSTALL%\lib\jdiff.jar;%CLASSPATH%
 
 java -classpath %CLASSPATH% jdiff.JDiff %*
 
