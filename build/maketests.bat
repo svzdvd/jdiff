b5d736f088ff2433a1a7cd065cf5fe60fc62c854
mdoar <mdoar>
2002-01-07 01:36:59 +0000

    Tests for anonymous classes added

--- build/maketests.bat
+++ build/maketests.bat
@@ -1,10 +1,10 @@
 cd ..\test
 
 REM Build the old API
-javac -classpath %JAVA_HOME%\lib\tools.jar;..\lib\xerces.jar;%CLASSPATH% old\RemovedPackage\*.java old\ChangedPackage\*.java old\ChangedPackageDoc\*.java old\ChangedPackageDoc2\*.java
+javac -classpath %JAVA_HOME%\lib\tools.jar;..\lib\xerces.jar;%CLASSPATH% old\RemovedPackage\*.java old\ChangedPackage\*.java old\ChangedPackageDoc\*.java old\ChangedPackageDoc2\*.java old\*.java
 
 REM Build the new API
-javac -classpath %JAVA_HOME%\lib\tools.jar;..\lib\xerces.jar;%CLASSPATH% new\AddedPackage\*.java new\ChangedPackage\*.java new\ChangedPackageDoc\*.java new\ChangedPackageDoc2\*.java
+javac -classpath %JAVA_HOME%\lib\tools.jar;..\lib\xerces.jar;%CLASSPATH% new\AddedPackage\*.java new\ChangedPackage\*.java new\ChangedPackageDoc\*.java new\ChangedPackageDoc2\*.java new\*.java
 
 REM Create jar files of the each version
 cd old
--- build/runtests.bat
+++ build/runtests.bat
@@ -6,26 +6,28 @@ set CLASSPATH=..\lib\xerces.jar;%CLASSPATH%
 
 REM These are the packages in each version of the API
 set OLDPKGS=RemovedPackage ChangedPackage ChangedPackageDoc ChangedPackageDoc2
+set OLDCLASSES=NoPackage NoPackageRemoved
 set NEWPKGS=AddedPackage ChangedPackage ChangedPackageDoc ChangedPackageDoc2
+set NEWCLASSES=NoPackage NoPackageAdded
 
 REM Generate Javadoc for the old API
 mkdir olddocs
 cd olddocs
-javadoc -private -sourcepath ..\old -doctitle "JDiff Test Old API" -windowtitle "JDiff Test Old API" %OLDPKGS%
+javadoc -private -sourcepath ..\old -doctitle "JDiff Test Old API" -windowtitle "JDiff Test Old API" %OLDPKGS% %OLDCLASSES%
 cd ..
 
 REM Generate Javadoc for the new API
 mkdir newdocs
 cd newdocs
-javadoc -private -sourcepath ..\new -doctitle "JDiff Test New API" -windowtitle "JDiff Test New API" %NEWPKGS%
+javadoc -private -sourcepath ..\new -doctitle "JDiff Test New API" -windowtitle "JDiff Test New API" %NEWPKGS% %NEWCLASSES%
 cd ..
 
 REM Generate XML for the old API. 
-javadoc -private -excludeclass private -excludemember private -J-Xmx128m -doclet jdiff.JDiff -docletpath ..\src -apiname "Old Test API" -sourcepath old %OLDPKGS%
+javadoc -private -excludeclass private -excludemember private -J-Xmx128m -doclet jdiff.JDiff -docletpath ..\src -apiname "Old Test API" -sourcepath old %OLDPKGS% %OLDCLASSES%
 
 REM Generate XML for the new API. This uses local copies of the DTD files.
-REM javadoc -private -excludeclass private -excludemember private -J-Xmx128m -doclet jdiff.JDiff -docletpath ..\src -apiname "New Test API" -baseURI "file:///C:/jdiff/lib" -sourcepath new %NEWPKGS%
-javadoc -private -excludeclass private -excludemember private -J-Xmx128m -doclet jdiff.JDiff -docletpath ..\src -apiname "New Test API" -sourcepath new %NEWPKGS%
+REM javadoc -private -excludeclass private -excludemember private -J-Xmx128m -doclet jdiff.JDiff -docletpath ..\src -apiname "New Test API" -baseURI "file:///C:/jdiff/lib" -sourcepath new %NEWPKGS% %NEWCLASSES%
+javadoc -private -excludeclass private -excludemember private -J-Xmx128m -doclet jdiff.JDiff -docletpath ..\src -apiname "New Test API" -sourcepath new %NEWPKGS% %NEWCLASSES%
 
 REM Generate an HTML report comparing the old and new APIs
 javadoc -J-Xmx128m -doclet jdiff.JDiff -docletpath ..\src -d newdocs -oldapi "Old Test API" -newapi "New Test API" -javadocold "../../olddocs/" -javadocnew "../../newdocs/" ..\lib\Null.java
