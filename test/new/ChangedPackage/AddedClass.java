273d087631d3cde2d999eb8471d5e2033deb8263
mdoar <mdoar>
2002-03-11 20:12:54 +0000

    Added an @since to check for missing @since tags

--- test/new/ChangedPackage/AddedClass.java
+++ test/new/ChangedPackage/AddedClass.java
@@ -1,6 +1,10 @@
 package ChangedPackage;
 
-/** This class is added in the new API. */
+/** 
+ * This class is added in the new API. 
+ *
+ * @since NewVersion
+ */
 public class AddedClass {
 
     public AddedClass() {
